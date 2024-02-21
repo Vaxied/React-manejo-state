109745b890aa77e160729e34c1966103e92b091f
Vaxied <psgmarcelo@gmail.com>
2024-02-21 19:36:47 -0400

    Individual state management with useState

--- src/App/UseState/index.js
+++ src/App/UseState/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 function UseState({ name }) {
+    const [error, setError] = React.useState(false)
     return (
         <div className="UseState-wrapper">
             <div className="UseState">
                 <h2>Eliminar UseState</h2>
                 <p>Por favor, escribe el codigo de seguridad.</p>
+                {error && <p>Error: el codigo es incorrecto</p>}
                 <input placeholder="Codigo de seguridad"></input>
-                <button>Confirmar</button>
+                <button onClick={() => setError(!error)}>Confirmar</button>
             </div>
         </div>
     )
