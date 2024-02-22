f42545a0f464586f07bed07a070673b2a0f005ea
Vaxied <psgmarcelo@gmail.com>
2024-02-22 17:43:52 -0400

    Imperative state flow with useState

--- src/App/UseState/index.js
+++ src/App/UseState/index.js
@@ -8,6 +8,8 @@ function UseState({ name }) {
         error: false,
         loading: false,
         value: '',
+        confirmed: false,
+        deleted: false,
     })
     const [error, setError] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
@@ -31,6 +33,7 @@ function UseState({ name }) {
                         ...state,
                         error: false,
                         loading: false,
+                        confirmed: true,
                     })
                 }
             }, 3000)
@@ -38,37 +41,93 @@ function UseState({ name }) {
         }
         console.log('terminando efecto')
     }, [state.loading])
-    return (
-        <div className="UseState-wrapper">
-            <div className="UseState">
-                <h2>Eliminar UseState</h2>
-                <p>Por favor, escribe el codigo de seguridad.</p>
-                {!!state.error && <p>Error: el codigo es incorrecto</p>}
-                {!!state.loading && <Loading />}
-                <input
-                    placeholder="Codigo de seguridad"
-                    value={state.value}
-                    onChange={() =>
-                        setState({
-                            ...state,
-                            value: event.target.value,
-                        })
-                    }
-                ></input>
-                <button
-                    onClick={() => {
-                        setState({
-                            ...state,
-                            error: false,
-                            loading: true,
-                        })
-                    }}
-                >
-                    Confirmar
-                </button>
+
+    console.log(state)
+    if (!!state.confirmed && !state.deleted) {
+        return (
+            <div className="UseState-wrapper">
+                <div className="UseState">
+                    <p>Estas seguro que deseas eliminar?</p>
+                    <button
+                        onClick={() =>
+                            setState({
+                                ...state,
+                                deleted: true,
+                            })
+                        }
+                    >
+                        Si, lo estoy
+                    </button>
+                    <button
+                        onClick={() =>
+                            setState({
+                                ...state,
+                                confirmed: false,
+                                value: '',
+                            })
+                        }
+                    >
+                        No, volver atras
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else if (!!state.confirmed && !!state.deleted) {
+        console.log('deleting')
+        return (
+            <>
+                <div className="UseState-wrapper">
+                    <div className="UseState">
+                        <p>Eliminado con exito!</p>
+                        <button
+                            onClick={() =>
+                                setState({
+                                    ...state,
+                                    confirmed: false,
+                                    deleted: false,
+                                    value: '',
+                                })
+                            }
+                        >
+                            Volver al estado inicial
+                        </button>
+                    </div>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <div className="UseState-wrapper">
+                <div className="UseState">
+                    <h2>Eliminar UseState</h2>
+                    <p>Por favor, escribe el codigo de seguridad.</p>
+                    {!!state.error && <p>Error: el codigo es incorrecto</p>}
+                    {!!state.loading && <Loading />}
+                    <input
+                        placeholder="Codigo de seguridad"
+                        value={state.value}
+                        onChange={() =>
+                            setState({
+                                ...state,
+                                value: event.target.value,
+                            })
+                        }
+                    ></input>
+                    <button
+                        onClick={() => {
+                            setState({
+                                ...state,
+                                error: false,
+                                loading: true,
+                            })
+                        }}
+                    >
+                        Confirmar
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default UseState
