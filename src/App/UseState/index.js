b0219c1d14e7a2d6b9b01255ae7273bd378f622c
Vaxied <psgmarcelo@gmail.com>
2024-02-21 22:20:38 -0400

    Independent states and state placement with useState

--- src/App/UseState/index.js
+++ src/App/UseState/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Loading from '../Loading/index'
 
+const SECURITY_CODE = 'paradigma'
+
 function UseState({ name }) {
     const [error, setError] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
+    const [value, setValue] = React.useState('')
 
+    console.log(value)
     React.useEffect(() => {
         console.log('iniciando efecto')
         // Simulating backend call
@@ -12,6 +16,9 @@ function UseState({ name }) {
             console.log('iniciando verificacion')
             setTimeout(() => {
                 setLoading(false)
+                if (SECURITY_CODE != value) {
+                    setError(true)
+                }
             }, 3000)
             console.log('finalizando verificacion')
         }
@@ -24,10 +31,17 @@ function UseState({ name }) {
                 <p>Por favor, escribe el codigo de seguridad.</p>
                 {!!error && <p>Error: el codigo es incorrecto</p>}
                 {!!loading && <Loading />}
-                <input placeholder="Codigo de seguridad"></input>
+                <input
+                    placeholder="Codigo de seguridad"
+                    value={value}
+                    onChange={() => setValue(event.target.value)}
+                ></input>
                 <button
-                    onClick={() => setLoading(true)}
-                    // or setError(prevState => !prevState)
+                    onClick={() => {
+                        setLoading(true)
+                        if (error) setError(false)
+                        // or setError(prevState => !prevState)
+                    }}
                 >
                     Confirmar
                 </button>
